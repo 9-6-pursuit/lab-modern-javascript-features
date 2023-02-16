83a00da6e75be3f1694da4e9f29bd4344d24d1ea
Judy <judychue@pursuit.org>
2023-02-16 15:27:23 -0500

    lab-modern-javascript-features-passed

--- index.js
+++ index.js
@@ -14,18 +14,20 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
+//Change the function to use arrow syntax  
+// function organizeMoonsByPlanet(moons)
+const organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
-    if (result[planet]) {
+// Use object destructuring to assign the `name` and `planet` variables.
+    const { name, planet } = moon ;
+  if (result[planet]) {
       result[planet].push(name);
     } else {
-      result[planet] = [name];
+      result[planet] =[name];
     }
-  }
-  return result;
+}
+return result;
 }
 
 /*
@@ -35,10 +37,10 @@ function organizeMoonsByPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
-
+//Change the function to use arrow syntax
+const getClosestToPlanet = (moons) => {
+//Use object destructuring and the rest operator to assign the `closest` and `rest` variables
+  let [closest, ...rest] = moons
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
       closest = moon;
@@ -56,7 +58,7 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
@@ -64,9 +66,9 @@ function createMoon(name, planet, distanceFromPlanetInKm) {
   distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
 
   return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
+    name,
+    planet,
+    distanceFromPlanetInKm
   };
 }
 
